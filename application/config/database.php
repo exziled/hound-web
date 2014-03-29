ec472fc692a5cc0da2964c9b66d96ce72a617756
Blake Bourque <Techplex.Engineer@gmail.com>
2014-03-29 18:39:14 -0400

    Setting up the database connection

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'srproj';
+$db['default']['password'] = 'houndplex';
+$db['default']['database'] = 'srproj';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
