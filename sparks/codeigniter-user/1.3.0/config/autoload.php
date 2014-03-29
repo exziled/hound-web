c34a22502cbc3573b5b21dd46faca5e4f2732d22
Blake Bourque <Techplex.Engineer@gmail.com>
2014-03-29 18:37:14 -0400

    fix the broken codeiniter-user to not have to copy over the library files

--- sparks/codeigniter-user/1.3.0/config/autoload.php
+++ sparks/codeigniter-user/1.3.0/config/autoload.php
@@ -1,3 +1,3 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database', 'user', 'user_manager');
