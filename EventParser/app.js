e9d0c142ce6aebe169f9e3c1a8712708c5339830
Blake Bourque <Techplex.Engineer@gmail.com>
2014-10-25 16:50:10 -0400

    Cleanup and document working code

--- EventParser/app.js
+++ EventParser/app.js
@@ -1,127 +1,80 @@
-var spark = require('spark');
 var request = require('request');
 var _ = require('underscore');
 
+// -----------------------------------------------------------------------------
+// High-Speed data from cores using websockets
+// -----------------------------------------------------------------------------
 
-// spark.login({accessToken: 'b122a221bf419da7491e4fca108f1835a2794451'}, function(err, body){
-// 	// console.log(err, body);
-// 	if (!err) {
-// 		spark.getEventStream(false, 'mine', function(data) {
-// 			console.log("Event: " + data);
-// 		});
-// 	} else {
-// 		console.log("ERROR: ",err);
-// 	}
-// });
-
-//settings
-
-// var PORT = 3000;
-
-// var app = require('http').createServer()
-// var io = require('socket.io')(app);
-// var fs = require('fs');
-
-// app.listen(PORT, function () {
-// 	console.log("Listening on port "+PORT);
-// });
-
-// function handler (req, res) {
-//   fs.readFile(__dirname + '/index.html',
-//   function (err, data) {
-//     if (err) {
-//       res.writeHead(500);
-//       return res.end('Error loading index.html');
-//     }
-
-//     res.writeHead(200);
-//     res.end(data);
-//   });
-// }
-
-// io.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
-
+// port to run the web socket server on which to listen for new data from the core.
 var wsport = 8080;
-var wsServer = require('ws').Server
-, wss = new wsServer({port: wsport});
+
+var wsServer = require('ws').Server;
+var wss = new wsServer({port: wsport});
+
+// variable to keep track of what web client is interested in what core.
+// This allows for easy lookup of subscribed web clients when the core sends new data.
+var coresock = {};
+// {"48ff6c065067555026311387": [socket, socket]}
 
 console.log("Core Server Listening on port "+wsport);
 
 wss.on('connection', function(ws) {
 	console.log('Client Connected');
 	ws.on('message', function(message) {
-		console.log('---\nrecieved: %s - size: %d', message, message.length);
+		//When data is recieved from a sparkcore emit the data to any subscribed
+		//web clients. try/catch for malformated json data.
 		try	{
 			message = JSON.parse(message);
-			// console.log(message.coreid);
 			var sockets = coresock[message.coreid];
 			for (var i = 0; sockets !== undefined && i < sockets.length; i++) {
 				sockets[i].emit('data', message);
-				// console.log("data");
-			};
-
+			}
 		} catch(err) {
 			console.log(err, message);
 		}
-		// console.log("");
 	});
 
 	ws.on('close', function() {
-		console.log('Client disconnected.');
+		console.log('SparkCore disconnected.');
 	});
-	ws.on('error', function() {
-		console.log('ERROR');
+	ws.on('error', function(err) {
+		console.log('SparkERROR', err);
 	});
-
-	// ws.send('ohaidere');
-
-	// setInterval(function() {
-	//    ws.send('delayed!');
-	// }, 2000);
 });
 
+// -----------------------------------------------------------------------------
+// High-Speed data pushed to socket.io clients
+// -----------------------------------------------------------------------------
+
+// Port to run socket.io on for web clients.
 var ioport = 2648;
 var io = require('socket.io')(ioport);
 console.log("Socket.IO Server Listening on port "+ioport);
 io.on('connection', function(socket){
-  console.log('a web user connected');
-  // console.log('socketid ',socket.id);
 
-  // socket.emit('message', "fred");
+	socket.on('who', function(coreid){
 
-  socket.on('who', function(coreid){
-	// console.log(coreid);
-	socket['coreid'] = coreid;
-	if (coresock[coreid] === undefined) {
-		coresock[coreid] = [socket];
-	} else {
-		coresock[coreid].push(socket);
-	}
-  })
-
-  socket.on('disconnect', function(){
-	var coreid = socket['coreid'];
-	console.log('web user disconnected', coreid);
-	var sockets = coresock[coreid];
-	for (var i = 0; i < sockets.length; i++) {
-		if (sockets[i].id == socket.id) {
-			sockets.splice(i,1); //remove the matching element
-			break;
+		console.log('socket.io web user connected', coreid);
+		socket['coreid'] = coreid;
+		if (coresock[coreid] === undefined) {
+			coresock[coreid] = [socket];
+		} else {
+			coresock[coreid].push(socket);
 		}
-	};
-	coresock[coreid] = sockets;
-  });
+	})
+
+	socket.on('disconnect', function(){
+		var coreid = socket['coreid'];
+		console.log('socket.io web user disconnected', coreid);
+
+		var sockets = coresock[coreid];
+		for (var i = 0; i < sockets.length; i++) {
+			if (sockets[i].id == socket.id) {
+				sockets.splice(i,1); //remove the matching element
+				break;
+			}
+		}
+		coresock[coreid] = sockets;
+	});
 
 });
-
-var coresock = {};
-// {"48ff6c065067555026311387": [socket, socket]}
-
-// setInterval(function () {
-// 	console.log(coresock);
-// }, 2000)
