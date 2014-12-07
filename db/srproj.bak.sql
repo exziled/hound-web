f32bdbd55b7c1a8e8c35e5770d92508d2b95b8ab
Blake Bourque <Techplex.Engineer@gmail.com>
2014-12-06 22:56:30 -0500

    Minimal database schema

--- db/srproj.bak.sql
+++ db/srproj.bak.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.0.6deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 30, 2014 at 12:47 PM
--- Server version: 5.5.38-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.4
+-- Generation Time: Dec 07, 2014 at 03:54 AM
+-- Server version: 5.5.37-0ubuntu0.13.10.1
+-- PHP Version: 5.5.3-1ubuntu2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -31,108 +31,28 @@ CREATE TABLE IF NOT EXISTS `devices` (
   `core_id` varchar(36) NOT NULL,
   `name` varchar(64) NOT NULL,
   `access_token` varchar(64) NOT NULL,
-  `form_factor` int(11) NOT NULL,
   `date_activated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id` int(11) DEFAULT NULL,
   `last_checkin` datetime NOT NULL,
   PRIMARY KEY (`device_id`),
-  KEY `fk_devices_users1_idx` (`user_id`),
-  KEY `fk_devices_form_factor1_idx` (`form_factor`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
-
---
--- Dumping data for table `devices`
---
-
-INSERT INTO `devices` (`device_id`, `core_id`, `name`, `access_token`, `form_factor`, `date_activated`, `user_id`, `last_checkin`) VALUES
-(2, '', 'Fred', '', 1, '2014-07-13 18:05:01', 1, '0000-00-00 00:00:00'),
-(3, '', 'Nope', '', 2, '2014-07-13 18:23:27', 4, '0000-00-00 00:00:00'),
-(4, 'dd', 'dd', 'dd', 0, '2014-07-13 21:16:19', 4, '0000-00-00 00:00:00'),
-(5, '', 'test', '', 0, '2014-07-19 15:17:33', 4, '0000-00-00 00:00:00'),
-(6, '', 'ted', '', 0, '2014-07-20 15:44:02', 4, '0000-00-00 00:00:00'),
-(7, '48ff6c065067555026311387', 'CorePlex', 'b122a221bf419da7491e4fca108f1835a2794451', 0, '2014-09-09 19:23:55', 4, '0000-00-00 00:00:00');
+  UNIQUE KEY `core_id` (`core_id`),
+  KEY `fk_devices_users1_idx` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `devices_device_group`
+-- Table structure for table `program`
 --
 
-CREATE TABLE IF NOT EXISTS `devices_device_group` (
-  `devices_device_group_id` int(11) NOT NULL AUTO_INCREMENT,
-  `device_id` int(11) DEFAULT NULL,
-  `device_group_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`devices_device_group_id`),
-  KEY `fk_devices_device_group_devices1_idx` (`device_id`),
-  KEY `fk_devices_device_group_device_group1_idx` (`device_group_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `devices_device_group`
---
-
-INSERT INTO `devices_device_group` (`devices_device_group_id`, `device_id`, `device_group_id`) VALUES
-(1, 2, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `device_group`
---
-
-CREATE TABLE IF NOT EXISTS `device_group` (
-  `device_group_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) DEFAULT NULL,
-  `locaton` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`device_group_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `device_group`
---
-
-INSERT INTO `device_group` (`device_group_id`, `name`, `locaton`) VALUES
-(1, 'your mom''s room', 'fu'),
-(2, 'Your mom''s room', 'joe');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `form_factor`
---
-
-CREATE TABLE IF NOT EXISTS `form_factor` (
-  `form_factor_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  `image_url` varchar(128) NOT NULL,
-  `socket_count` int(11) NOT NULL,
-  `release_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `description` varchar(256) NOT NULL,
-  PRIMARY KEY (`form_factor_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `form_factor`
---
-
-INSERT INTO `form_factor` (`form_factor_id`, `name`, `image_url`, `socket_count`, `release_date`, `description`) VALUES
-(0, 'Unknown', '', 0, '2014-07-20 15:53:53', ''),
-(1, 'Outlet', '', 2, '2014-07-13 20:35:55', ''),
-(2, 'Power Strip', '', 10, '2014-07-13 20:36:01', '');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `permissions`
---
-
-CREATE TABLE IF NOT EXISTS `permissions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(12) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `program` (
+  `device_id` int(11) NOT NULL,
+  `xml` text NOT NULL,
+  `javascript` text NOT NULL,
+  PRIMARY KEY (`device_id`),
+  UNIQUE KEY `device_id_2` (`device_id`),
+  KEY `device_id` (`device_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -142,7 +62,9 @@ CREATE TABLE IF NOT EXISTS `permissions` (
 
 CREATE TABLE IF NOT EXISTS `samples` (
   `sample_id` int(11) NOT NULL AUTO_INCREMENT,
-  `socket_id` int(11) NOT NULL,
+  `socket` int(11) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `device_id` int(11) NOT NULL,
   `current` double NOT NULL,
   `voltage` double NOT NULL,
   `powerfactor` float NOT NULL,
@@ -152,37 +74,8 @@ CREATE TABLE IF NOT EXISTS `samples` (
   `apparent_power` double NOT NULL,
   `real_power` double NOT NULL,
   PRIMARY KEY (`sample_id`),
-  KEY `fk_samples_sockets1_idx` (`socket_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `samples`
---
-
-INSERT INTO `samples` (`sample_id`, `socket_id`, `current`, `voltage`, `powerfactor`, `frequency`, `temperature`, `wifi_strength`, `apparent_power`, `real_power`) VALUES
-(2, 1, 0, 0, 0, 0, 0, 0, 0, 0),
-(3, 1, 0, 0, 0, 0, 0, 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sockets`
---
-
-CREATE TABLE IF NOT EXISTS `sockets` (
-  `socket_id` int(11) NOT NULL AUTO_INCREMENT,
-  `device_id` int(11) NOT NULL,
-  `state` tinyint(1) NOT NULL,
-  PRIMARY KEY (`socket_id`),
-  KEY `fk_sockets_devices1_idx` (`device_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `sockets`
---
-
-INSERT INTO `sockets` (`socket_id`, `device_id`, `state`) VALUES
-(1, 7, 1);
+  KEY `fk_samples_sockets1_idx` (`device_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1333 ;
 
 -- --------------------------------------------------------
 
@@ -199,44 +92,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `last_login` date DEFAULT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `login`, `password`, `last_login`, `active`) VALUES
-(1, 'Administrator', 'admin@localhost', 'admin', '$2a$12$SR04o2/JNV5ZoVGZNgPiiezqM2f5D0eVDXsSDoWcfQqg/mST6O6Ye', '2014-09-23', 1),
-(4, '', '', 'techplex', '$2a$12$x0swrPtQ3g1BRwyu8VuK9.4nQC95tHj6BsDFLKcKjYwT79AJrHOOa', '2014-09-30', 1),
-(5, '', '', 'blake', '$2a$12$bciy5TOZYEyP3pGyjwgH7eOVMA7Mu.PHBKrCCuHjqnH8AvRm9VPvi', '2014-09-29', 1),
-(6, '', '', 'dickandballs', '$2a$12$6xMBLTsEW9KFEryVsjnXmu23zBeOq2w/ug6gp96G09T7BEEddMEB.', '2014-09-30', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users_meta`
---
-
-CREATE TABLE IF NOT EXISTS `users_meta` (
-  `user_id` int(11) NOT NULL,
-  `name` varchar(40) NOT NULL,
-  `value` varchar(255) NOT NULL,
-  KEY `user_id` (`user_id`),
-  KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users_permissions`
---
-
-CREATE TABLE IF NOT EXISTS `users_permissions` (
-  `user_id` int(11) NOT NULL,
-  `permission_id` int(11) NOT NULL,
-  KEY `user_id` (`user_id`),
-  KEY `permission_id` (`permission_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Constraints for dumped tables
@@ -246,45 +102,19 @@ CREATE TABLE IF NOT EXISTS `users_permissions` (
 -- Constraints for table `devices`
 --
 ALTER TABLE `devices`
-  ADD CONSTRAINT `devices_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `devices_ibfk_2` FOREIGN KEY (`form_factor`) REFERENCES `form_factor` (`form_factor_id`);
+  ADD CONSTRAINT `devices_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `devices_device_group`
+-- Constraints for table `program`
 --
-ALTER TABLE `devices_device_group`
-  ADD CONSTRAINT `devices_device_group_ibfk_1` FOREIGN KEY (`device_group_id`) REFERENCES `device_group` (`device_group_id`),
-  ADD CONSTRAINT `devices_device_group_ibfk_2` FOREIGN KEY (`device_id`) REFERENCES `devices` (`device_id`);
-
---
--- Constraints for table `permissions`
---
-ALTER TABLE `permissions`
-  ADD CONSTRAINT `permissions_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users_permissions` (`permission_id`);
+ALTER TABLE `program`
+  ADD CONSTRAINT `program_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`device_id`);
 
 --
 -- Constraints for table `samples`
 --
 ALTER TABLE `samples`
-  ADD CONSTRAINT `samples_ibfk_1` FOREIGN KEY (`socket_id`) REFERENCES `sockets` (`socket_id`);
-
---
--- Constraints for table `sockets`
---
-ALTER TABLE `sockets`
-  ADD CONSTRAINT `sockets_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`device_id`);
-
---
--- Constraints for table `users_meta`
---
-ALTER TABLE `users_meta`
-  ADD CONSTRAINT `users_meta_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `users_permissions`
---
-ALTER TABLE `users_permissions`
-  ADD CONSTRAINT `users_permissions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `samples_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`device_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
